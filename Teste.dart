0497800339812da0a016a63061349bababff7744
matheus rocha <matheusfirefly@gmail.com>
2023-08-07 23:22:30 -0300

    Getter e setters implemantados

--- Teste.dart
+++ Teste.dart
@@ -1,4 +1,12 @@
 class Teste {
   var estudante;
   var nota;
+   String getEstudante() => this.estudante;
+
+  void setEstudante(var estudante) => this.estudante = estudante;
+
+  int getNota() => this.nota;
+
+  void setNota(var nota) => this.nota = nota;
 }
+
